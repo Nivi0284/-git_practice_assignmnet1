2e4691f52b89be4885eaaa69502e7d1f18f5c938
Nivi0284 <nivijaiswal45@gmail.com>
2022-09-20 23:24:35 +0530

    added code

---
+++ prime.js
@@ -0,0 +1,16 @@
+function Prime(num){
+    let factors=0;
+    for(let i=1; i<=num; i++){
+        if(num%i==0){
+            factors++;
+        }
+        return false;
+    }
+}
+
+let answer=Prime(19);
+if(answer==true){
+    console.log("prime");
+}else{
+    console.log("Not prime");
+}
